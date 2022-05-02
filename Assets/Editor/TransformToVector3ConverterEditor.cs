4265320ced845ce7f42d80436d36ffa9eb5ad411
AndreyBirchenko <andrey_birchenko@mail.ru>
2022-05-02 22:43:41 +0300

    Init

---
+++ Assets/Editor/TransformToVector3ConverterEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+using UnityEngine;
+
+namespace Utility
+{
+    [CustomEditor(typeof(TransformToVector3Converter))]
+    public class TransformToVector3ConverterEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var script = (TransformToVector3Converter) target;
+
+            if (GUILayout.Button("Convert"))
+            {
+                script.Convert();
+            }
+        }
+    }
+}
