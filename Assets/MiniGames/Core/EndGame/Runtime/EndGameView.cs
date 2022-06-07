817edc78ae19e71b80fcec2471dcf75b709f8139
AndreyBirchenko <andrey_birchenko@mail.ru>
2022-06-08 02:09:59 +0300

    [EndGame] Add appear animation

--- Assets/MiniGames/Core/EndGame/Runtime/EndGameView.cs
+++ Assets/MiniGames/Core/EndGame/Runtime/EndGameView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using DG.Tweening;
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,20 +12,41 @@ namespace Core.Services.Toolbar.Views
         [SerializeField] private GameObject _panel;
         [SerializeField] private Button _restartButton;
         [SerializeField] private Button _quitButton;
-        
+
+        private Sequence _sequence;
+
         public void Show()
         {
-            _panel.SetActive(true);
+            var initScale = _panel.transform.localScale;
+            var animInterval = 0.8f;
+
+            _sequence = GetSequence();
+            _sequence
+                .Append(_panel.transform.DOScale(Vector3.zero, 0))
+                .AppendCallback(() => _panel.SetActive(true))
+                .Append(_panel.transform.DOScale(initScale * 1.25f, animInterval / 2))
+                .Append(_panel.transform.DOScale(initScale, animInterval / 2));
         }
 
         public void SubscribeRestartButton(Action call)
         {
             _restartButton.onClick.AddListener(call.Invoke);
         }
-        
+
         public void SubscribeQuitButton(Action call)
         {
             _quitButton.onClick.AddListener(call.Invoke);
+        }
+
+        private Sequence GetSequence()
+        {
+            if (_sequence.IsActive() && _sequence.IsPlaying())
+            {
+                _sequence.Kill();
+                _sequence = null;
+            }
+
+            return DOTween.Sequence();
         }
     }
 }
